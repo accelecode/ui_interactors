c80da1a514adccb63258ecd86596efd9331c5e69
Kevin Rood <kevin.rood@accelecode.com>
2018-03-21 11:43:46 -0400

    UiSteps delegates to root_view

--- src/test/tests/ui_steps.rb
+++ src/test/tests/ui_steps.rb
@@ -1,6 +1,11 @@
+require 'forwardable'
+
 class UiSteps
+  extend Forwardable
 
   attr_reader :driver, :root_view
+
+  def_delegators :root_view, :view, :action, :text, :list, :text_field, :dropdown_field, :checkbox_field
 
   def initialize(driver, root_view)
     @driver = driver
